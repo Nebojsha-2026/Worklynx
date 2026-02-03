015501c0e5425462c0d20304a39181d32d9d776a
Nebojsha-2026 <work.petreski@gmail.com>
2026-02-04 00:08:08 +1100

    Update supabase.js

--- js/core/supabase.js
+++ js/core/supabase.js
@@ -1,1 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
+export const supabase = createClient(
+  "https://kufuvxifxdnusnbbgdvw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1ZnV2eGlmeGRudXNuYmJnZHZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMTgwNzQsImV4cCI6MjA4NTY5NDA3NH0.Jo6uzx64ZgkgUBY7HfvAsge_7JDpZkD9MGUbcGoiUoU"
+);
