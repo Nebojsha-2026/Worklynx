c466873d49f4118258995b03284be8e81e56316f
Nebojsha-2026 <work.petreski@gmail.com>
2026-02-04 00:09:35 +1100

    Update login.js

--- js/pages/login.js
+++ js/pages/login.js
@@ -1,1 +1,12 @@
+import { login } from "../core/auth.js";
 
+document.querySelector("#loginForm").addEventListener("submit", async e => {
+  e.preventDefault();
+  const email = email.value;
+  const password = password.value;
+
+  const { error } = await login(email, password);
+  if (error) return alert(error.message);
+
+  window.location.href = "/manager.html";
+});
