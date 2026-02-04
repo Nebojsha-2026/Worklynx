2310eb63f758a11d1a43288762d9f8d1329cf2c2
Nebojsha-2026 <work.petreski@gmail.com>
2026-02-04 16:04:29 +1100

    Delete js/pages/manager.js

--- js/pages/manager.js
+++
@@ -1,6 +0,0 @@
-import { protect } from "../core/router.js";
-import { logout } from "../core/auth.js";
-
-const profile = await protect("manager");
-
-document.querySelector("#logoutBtn").onclick = logout;
